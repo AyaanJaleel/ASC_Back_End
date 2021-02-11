6dd2268a61da2471a36f80e57045e7446f5ebea2
Ayaan Jaleel <ayaanjaleel@Ayaans-MacBook-Pro.local>
2021-02-11 16:46:35 +0400

    setHeader fixing

--- server.js
+++ server.js
@@ -18,9 +18,22 @@ app.use((req,res,next)=>{
     // res.header('Access-Control-Allow-Headers', '*');
     // res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS');
     // next();
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    // res.setHeader('Access-Control-Allow-Methods', 'PUT');
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'PUT');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
 });
 
 app.use(function(req, res, next) { 
